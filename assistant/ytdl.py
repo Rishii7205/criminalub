e42fe201163cbf0790b46cb0c320b19cd7ad8d68
Rishii_x_Janni <118364756+Rishii7205@users.noreply.github.com>
2024-01-31 23:28:29 +0530

    Update ytdl.py

--- assistant/ytdl.py
+++ assistant/ytdl.py
@@ -36,7 +36,7 @@ except ImportError:
     VideosSearch = None
 
 
-ytt = "https://graph.org/file/afd04510c13914a06dd03.jpg"
+ytt = "https://graph.org/file/2f160e70e4b2807f5e3af.jpg"
 _yt_base_url = "https://www.youtube.com/watch?v="
 BACK_BUTTON = {}
 
